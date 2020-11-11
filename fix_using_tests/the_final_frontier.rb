13562a1ce533b1e995175b64031fda00ea010276
Dustin Rothschild <drothschild89@gmail.com>
2020-11-11 21:14:20 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-require 'pry'
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -14,5 +14,3 @@ def engage(state_log, generate_star_date)
   date = generate_star_date
   return date
 end
-
-binding.pry
